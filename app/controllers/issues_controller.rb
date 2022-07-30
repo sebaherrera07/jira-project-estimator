a856b0c6b065f3ade3ad3174600f36a138b88e80
Sebastian Herrera <sebaherrera93@gmail.com>
2022-07-30 20:42:16 -0300

    refactor views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,12 +4,25 @@ class IssuesController < ApplicationController
   def index
     @project_key = params[:project_id]
     @epic_key = params[:epic_id]
-    @issues = jira_epic_issues(@project_key, @epic_key, params[:labels])
+    epic = jira_project_epic(@project_key, @epic_key)
+    epic_issues = jira_epic_issues(@project_key, @epic_key, params[:labels])
+    @epic_presenter = EpicPresenter.new(
+      epic: epic,
+      issues: epic_issues
+    )
   end
 
   private
 
+  def jira_project_epic(project_key, epic_key)
+    jira_api_client_service.query_project_epic(project_key, epic_key)
+  end
+
   def jira_epic_issues(project_key, epic_key, labels)
-    JiraApiClientService.new.query_epic_issues(project_key, epic_key, labels)
+    jira_api_client_service.query_epic_issues(project_key, epic_key, labels)
+  end
+
+  def jira_api_client_service
+    @jira_api_client_service ||= JiraApiClientService.new
   end
 end
