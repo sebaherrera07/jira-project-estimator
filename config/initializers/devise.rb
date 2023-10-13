baef0ce2529e8f087829e29c1667cc151e44b3bf
Sebastian Herrera <sebastian@rainforestapp.com>
2023-10-13 14:59:38 +0300

    Fix Devise error alerts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,6 +305,15 @@ Devise.setup do |config|
 
   # ==> Configuration for :registerable
 
+  # ==> Hotwire/Turbo configuration
+  # When using Devise with Hotwire/Turbo, the http status for error responses
+  # and some redirects must match the following. The default in Devise for existing
+  # apps is `200 OK` and `302 Found respectively`, but new apps are generated with
+  # these new defaults that match Hotwire/Turbo behavior.
+  # Note: These might become the new default in future versions of Devise.
+  config.responder.error_status = :unprocessable_entity
+  config.responder.redirect_status = :see_other
+
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
