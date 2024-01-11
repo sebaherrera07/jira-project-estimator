07fb47c6e95412a96bf45e288cdf3b9f115b58aa
Sebastian Herrera <sebastian@rainforestapp.com>
2024-01-11 17:59:25 -0300

    Allow to set category for simulated estimation

--- app/objects/estimation_category.rb
+++ app/objects/estimation_category.rb
@@ -4,20 +4,28 @@ class EstimationCategory
   PESSIMISTIC = :pessimistic
   NEUTRAL = :neutral
   OPTIMISTIC = :optimistic
-  ACTUAL_SINCE_BEGINNING = :actual_since_beginning
-  ACTUAL_SINCE_LAST_3_WEEKS = :actual_since_last_3_weeks
+  AVERAGE_SINCE_BEGINNING = :avg_since_beginning
+  AVERAGE_LAST_3_WEEKS = :avg_last_3_weeks
 
   CATEGORIES = {
     PESSIMISTIC => 0,
     NEUTRAL => 1,
     OPTIMISTIC => 2,
-    ACTUAL_SINCE_BEGINNING => 3,
-    ACTUAL_SINCE_LAST_3_WEEKS => 4
+    AVERAGE_SINCE_BEGINNING => 3,
+    AVERAGE_LAST_3_WEEKS => 4
+  }.with_indifferent_access.freeze
+
+  CATEGORIES_TITLE = {
+    PESSIMISTIC => PESSIMISTIC.to_s.humanize,
+    NEUTRAL => NEUTRAL.to_s.humanize,
+    OPTIMISTIC => OPTIMISTIC.to_s.humanize,
+    AVERAGE_SINCE_BEGINNING => 'Avg. since beginning',
+    AVERAGE_LAST_3_WEEKS => 'Avg. last 3 weeks'
   }.with_indifferent_access.freeze
 
   def self.dropdown_options
-    CATEGORIES.except(ACTUAL_SINCE_BEGINNING, ACTUAL_SINCE_LAST_3_WEEKS).keys.map do |category|
-      [category.humanize, category]
+    [NEUTRAL, PESSIMISTIC, OPTIMISTIC].map do |category|
+      [CATEGORIES_TITLE[category], category.to_s]
     end
   end
 end
